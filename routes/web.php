d22351ed5fe33af95cf8938440f46fc6de5e23bd
Willzin1 <william.mendonca34@gmail.com>
2025-05-10 16:41:22 -0300

    Testando coisas para realizar o deploy

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'Home da API, por favor utilize os endpoints descritos na documentação!']);
 });
