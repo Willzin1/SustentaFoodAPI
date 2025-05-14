e80ac31ad9eb1f2cd10335e50eb474ae7bc2f165
Willzin1 <william.mendonca34@gmail.com>
2025-05-14 20:42:30 -0300

    Fix: fixing error where no data was being returned

--- app/Helpers/ReservasHelper.php
+++ app/Helpers/ReservasHelper.php
@@ -39,39 +39,40 @@ class ReservasHelper
     public static function applySearchFilter($request, $query)
     {
         if ($request->has('search')) {
-        $search = $request->input('search');
-        $filter = $request->input('filter');
+            $search = $request->input('search');
+            $filter = $request->input('filter');
 
-        switch ($filter) {
-            case 'ID':
-                $query->where('id', 'like', "%$search%");
-                break;
-            case 'Nome':
-                $query->whereHas('user', function ($q) use ($search) {
-                    $q->where('name', 'like', "%$search%");
-                });
-                break;
-            case 'Data':
-                $query->where('data', 'like', "%$search%");
-                break;
-            case 'Hora':
-                $query->where('hora', 'like', "%$search%");
-                break;
-            case 'Quantidade':
-                $query->where('quantidade_cadeiras', 'like', "%$search%");
-                break;
-            default:
-                $query->where(function ($q) use ($search) {
-                    $q->where('id', 'like', "%$search%")
-                        ->orWhere('data', 'like', "%$search%")
-                        ->orWhere('hora', 'like', "%$search%")
-                        ->orWhere('quantidade_cadeiras', 'like', "%$search%")
-                        ->orWhereHas('user', function ($q2) use ($search) {
-                            $q2->where('name', 'like', "%$search%");
-                        });
-                });
+            switch ($filter) {
+                case 'ID':
+                    $query->where('id', 'like', "%$search%");
+                    break;
+                case 'Nome':
+                    $query->whereHas('user', function ($q) use ($search) {
+                        $q->where('name', 'like', "%$search%");
+                    });
+                    break;
+                case 'Data':
+                    $query->where('data', 'like', "%$search%");
+                    break;
+                case 'Hora':
+                    $query->where('hora', 'like', "%$search%");
+                    break;
+                case 'Quantidade':
+                    $query->where('quantidade_cadeiras', 'like', "%$search%");
+                    break;
+                default:
+                    $query->where(function ($q) use ($search) {
+                        $q->where('id', 'like', "%$search%")
+                            ->orWhere('data', 'like', "%$search%")
+                            ->orWhere('hora', 'like', "%$search%")
+                            ->orWhere('quantidade_cadeiras', 'like', "%$search%")
+                            ->orWhereHas('user', function ($q2) use ($search) {
+                                $q2->where('name', 'like', "%$search%");
+                            });
+                    });
+            }
         }
-    }
+
         return $query;
     }
 }
