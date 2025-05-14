3aa7b204e6a04d21f72a95a50a998d17afe3fa22
Willzin1 <william.mendonca34@gmail.com>
2025-05-14 19:43:56 -0300

    Fix: fixing error where no data was being returned

--- app/Http/Controllers/Api/RelatorioReservaController.php
+++ app/Http/Controllers/Api/RelatorioReservaController.php
@@ -14,6 +14,42 @@ class RelatorioReservaController extends Controller
         $query = Reserva::with('user')->whereDate('data', Carbon::today())->orderBy('data');
         $total = $query->count();
 
+
+        if (request()->has('search')) {
+            $search = request('search');
+            $filter = request('filter');
+
+            switch ($filter) {
+                case 'ID':
+                    $query->where('id', 'like', "%$search%");
+                    break;
+                case 'Nome':
+                    $query->whereHas('user', function ($q) use ($search) {
+                        $q->where('name', 'like', "%$search%");
+                    });
+                    break;
+                case 'Data':
+                    $query->where('data', 'like', "%$search%");
+                    break;
+                case 'Hora':
+                    $query->where('hora', 'like', "%$search%");
+                    break;
+                case 'Quantidade':
+                    $query->where('quantidade_cadeiras', 'like', "%$search%");
+                    break;
+                default:
+                    $query->where(function ($q) use ($search) {
+                        $q->where('id', 'like', "%$search%")
+                          ->orWhere('data', 'like', "%$search%")
+                          ->orWhere('hora', 'like', "%$search%")
+                          ->orWhere('quantidade_cadeiras', 'like', "%$search%")
+                          ->orWhereHas('user', function ($q2) use ($search) {
+                              $q2->where('name', 'like', "%$search%");
+                            });
+                    });
+            }
+        }
+
         $todayReservations = $query->paginate(5, ['id', 'user_id', 'data', 'hora', 'quantidade_cadeiras', 'name', 'email']);
 
         return response()->json(['total' => $total, 'reservas' => $todayReservations]);
@@ -27,6 +63,43 @@ class RelatorioReservaController extends Controller
         $query = Reserva::with('user')->whereBetween('data', [$startWeek, $endWeek])->orderBy('data');
         $total = $query->count();
 
+
+
+        if (request()->has('search')) {
+            $search = request('search');
+            $filter = request('filter');
+
+            switch ($filter) {
+                case 'ID':
+                    $query->where('id', 'like', "%$search%");
+                    break;
+                case 'Nome':
+                    $query->whereHas('user', function ($q) use ($search) {
+                        $q->where('name', 'like', "%$search%");
+                    });
+                    break;
+                case 'Data':
+                    $query->where('data', 'like', "%$search%");
+                    break;
+                case 'Hora':
+                    $query->where('hora', 'like', "%$search%");
+                    break;
+                case 'Quantidade':
+                    $query->where('quantidade_cadeiras', 'like', "%$search%");
+                    break;
+                default:
+                    $query->where(function ($q) use ($search) {
+                        $q->where('id', 'like', "%$search%")
+                          ->orWhere('data', 'like', "%$search%")
+                          ->orWhere('hora', 'like', "%$search%")
+                          ->orWhere('quantidade_cadeiras', 'like', "%$search%")
+                          ->orWhereHas('user', function ($q2) use ($search) {
+                              $q2->where('name', 'like', "%$search%");
+                            });
+                    });
+            }
+        }
+
         $weekReservations = $query->paginate(5, ['id', 'user_id', 'data', 'hora', 'quantidade_cadeiras', 'name', 'email']);
 
         return response()->json(['total' => $total, 'reservas' => $weekReservations], 200);
@@ -39,6 +112,42 @@ class RelatorioReservaController extends Controller
 
         $query = Reserva::with('user')->whereBetween('data', [$startMonth, $endMonth])->orderBy('data');
         $total = $query->count();
+
+
+        if (request()->has('search')) {
+            $search = request('search');
+            $filter = request('filter');
+
+            switch ($filter) {
+                case 'ID':
+                    $query->where('id', 'like', "%$search%");
+                    break;
+                case 'Nome':
+                    $query->whereHas('user', function ($q) use ($search) {
+                        $q->where('name', 'like', "%$search%");
+                    });
+                    break;
+                case 'Data':
+                    $query->where('data', 'like', "%$search%");
+                    break;
+                case 'Hora':
+                    $query->where('hora', 'like', "%$search%");
+                    break;
+                case 'Quantidade':
+                    $query->where('quantidade_cadeiras', 'like', "%$search%");
+                    break;
+                default:
+                    $query->where(function ($q) use ($search) {
+                        $q->where('id', 'like', "%$search%")
+                          ->orWhere('data', 'like', "%$search%")
+                          ->orWhere('hora', 'like', "%$search%")
+                          ->orWhere('quantidade_cadeiras', 'like', "%$search%")
+                          ->orWhereHas('user', function ($q2) use ($search) {
+                              $q2->where('name', 'like', "%$search%");
+                            });
+                    });
+            }
+        }
 
         $monthReservations = $query->paginate(5, ['id', 'user_id', 'data', 'hora', 'quantidade_cadeiras', 'name', 'email']);
 
