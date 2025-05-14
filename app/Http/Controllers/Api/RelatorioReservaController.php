06337406000a55f71a62655acf8e5e70986be7e3
Willzin1 <william.mendonca34@gmail.com>
2025-05-14 20:33:27 -0300

    Fix: fixing error where no data was being returned

--- app/Http/Controllers/Api/RelatorioReservaController.php
+++ app/Http/Controllers/Api/RelatorioReservaController.php
@@ -23,7 +23,7 @@ class RelatorioReservaController extends Controller
         return response()->json(['total' => $total, 'reservas' => $todayReservations]);
     }
 
-    public function getReservationsByWeek(): JsonResponse
+    public function getReservationsByWeek(Request $request): JsonResponse
     {
         $startWeek = Carbon::now()->startOfWeek();
         $endWeek = Carbon::now()->endOfWeek();
@@ -31,42 +31,7 @@ class RelatorioReservaController extends Controller
         $query = Reserva::with('user')->whereBetween('data', [$startWeek, $endWeek])->orderBy('data');
         $total = $query->count();
 
-
-
-        if (request()->has('search')) {
-            $search = request('search');
-            $filter = request('filter');
-
-            switch ($filter) {
-                case 'ID':
-                    $query->where('id', 'like', "%$search%");
-                    break;
-                case 'Nome':
-                    $query->whereHas('user', function ($q) use ($search) {
-                        $q->where('name', 'like', "%$search%");
-                    });
-                    break;
-                case 'Data':
-                    $query->where('data', 'like', "%$search%");
-                    break;
-                case 'Hora':
-                    $query->where('hora', 'like', "%$search%");
-                    break;
-                case 'Quantidade':
-                    $query->where('quantidade_cadeiras', 'like', "%$search%");
-                    break;
-                default:
-                    $query->where(function ($q) use ($search) {
-                        $q->where('id', 'like', "%$search%")
-                          ->orWhere('data', 'like', "%$search%")
-                          ->orWhere('hora', 'like', "%$search%")
-                          ->orWhere('quantidade_cadeiras', 'like', "%$search%")
-                          ->orWhereHas('user', function ($q2) use ($search) {
-                              $q2->where('name', 'like', "%$search%");
-                            });
-                    });
-            }
-        }
+        ReservasHelper::applySearchFilter($request, $query);
 
         $weekReservations = $query->paginate(5, ['id', 'user_id', 'data', 'hora', 'quantidade_cadeiras', 'name', 'email']);
 
